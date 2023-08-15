b0be50abf0ee09f435cb5bc795685f0a5eef327b
Jonatan <jotan.alexander@gmail.com>
2023-08-15 11:48:28 -0500

    finish app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,22 @@
-// botón de "Hacia arriba"
+// button scroll to top
+// control of the button is obtained by means of the id
+let myButton = document.getElementById('scrollTop')
+
+// parameters are defined for the button to appear after scrolling above a certain value
+window.onscroll = function() {
+    scrollFunction()
+}
+
+let scrollFunction = () => {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 300) {
+        myButton.style.display = 'block';
+    } else {
+        myButton.style.display = 'none';
+    }
+}
+
+// function for the button to go to the top of the page
+const topFunction = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
